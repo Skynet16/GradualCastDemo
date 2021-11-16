82868208d17f68cd7215f7b01eae9d223ef373cb
Skynet_u <Vanes_kote@mail.ru>
2021-11-16 11:01:29 +0300

    GradualCastDemo

---
+++ src/com/company/GradualCastDemo.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class GradualCastDemo {
+    public static void main(String[] args) {
+
+        byte x, z;
+        int y;
+
+        x = 5;
+        y = x * x;
+
+        z = (byte) (x * x);
+
+    }
+}
